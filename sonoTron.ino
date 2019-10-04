a82049218490770c6b28844bb60b2052b2c19db2
Erik Dorstel <43003063+ErikDorstel@users.noreply.github.com>
2019-10-04 15:00:44 +0200

    Add files via upload

--- sonoTron.ino
+++ sonoTron.ino
@@ -107,34 +107,98 @@ AudioConnection          patchCord85(filter12[11], 1, peak[11], 0);
 AudioConnection          patchCord86(filter12[12], 2, peak[12], 0);
 AudioControlSGTL5000     sgtl5000_1;
 
-Metro timer1 = Metro(5);
-Metro timer2 = Metro(1000);
+Metro timer = Metro(1000);
+float modulatorLevel[13], carrierGain[13];
+float envelopeSpeed=0.99588, envelopeLimit=0.1;
+float modulatorButtom=50, modulatorTop=5000, carrierButtom=50, carrierTop=5000;
 
 void setup() {
-  float freq;
   Serial.begin(9600);
+  Serial3.begin(31250,SERIAL_8N1);
   AudioMemory(50);
   sgtl5000_1.enable(); sgtl5000_1.volume(0.8); sgtl5000_1.unmuteLineout(); sgtl5000_1.lineOutLevel(29);
   sgtl5000_1.inputSelect(AUDIO_INPUT_LINEIN); sgtl5000_1.lineInLevel(5);
   noise1.amplitude(1); pink1.amplitude(1);
-  mixer1.gain(0,1); mixer1.gain(1,0); mixer1.gain(2,0); mixer1.gain(3,0);
-  mixer2.gain(0,0); mixer2.gain(1,0); mixer2.gain(2,1); mixer2.gain(3,0);
-  mixer6.gain(0,5); mixer6.gain(1,5); mixer6.gain(2,5); mixer6.gain(3,0);
-  for (byte c=1;c<=12;c++) {
-    freq=50.0*pow(1000/50.0,(c-1)/11.0); Serial.print(freq); Serial.print(" - ");
-    filter11[c].frequency(freq); filter11[c].resonance(1); filter11[c].octaveControl(1);
-    filter12[c].frequency(freq); filter12[c].resonance(1); filter12[c].octaveControl(1);
-    filter21[c].frequency(freq); filter21[c].resonance(1); filter21[c].octaveControl(1);
-    filter22[c].frequency(freq); filter22[c].resonance(1); filter22[c].octaveControl(1); }
-  delay(1000); Serial.println(""); }
+  for (byte c=1;c<=12;c++) { carrierGain[c]=1; }
+  setModulatorSource(1,0,0);
+  setCarrierSource(1,0,0);
+  setModulatorFrequency(50,5000);
+  setCarrierFrequency(50,5000);
+  setFilterResonance(5);
+  setOutputAmplifier(2);
+  delay(1000); }
 
 void loop() {
-  if (timer1.check() == 1) {
-      mixer3.gain(0,peak[1].read()); mixer3.gain(1,peak[2].read()); mixer3.gain(2,peak[7].read()); mixer3.gain(3,peak[8].read());
-      mixer4.gain(0,peak[3].read()); mixer4.gain(1,peak[4].read()); mixer4.gain(2,peak[9].read()); mixer4.gain(3,peak[10].read());
-      mixer5.gain(0,peak[5].read()); mixer5.gain(1,peak[6].read()); mixer5.gain(2,peak[11].read()); mixer5.gain(3,peak[12].read()); }
-  if (timer2.check() == 1) { Serial.print(peak1.read()); Serial.print(" - "); Serial.print(peak2.read()); Serial.print(" - "); Serial.print(peak3.read()); Serial.println(""); } }
+  byte MIDIinA; static byte MIDIstatusA=0, MIDIchannelA=0, MIDIpara1A=0, MIDIpara2A=0;
+  if (Serial3.available() > 0) { MIDIinA = Serial3.read();
+    if ((MIDIinA & 128) == 128) { MIDIstatusA=MIDIinA & 240; MIDIchannelA=MIDIinA & 15; MIDIpara1A=255; MIDIpara2A=255; } else {
+    if (MIDIpara1A==255) { MIDIpara1A=MIDIinA; } else if (MIDIpara2A==255) { MIDIpara2A=MIDIinA;
+      // if (MIDIstatusA==0x80) { MIDIsetNoteOff(MIDIchannelA,MIDIpara1A,MIDIpara2A); }
+      // if (MIDIstatusA==0x90) { MIDIsetNoteOn(MIDIchannelA,MIDIpara1A,MIDIpara2A); }
+      if (MIDIstatusA==0xB0) { MIDIsetControl(MIDIchannelA,MIDIpara1A,MIDIpara2A); }
+      // if (MIDIstatusA==0xE0) { MIDIsetPitchbend(MIDIchannelA,MIDIpara1A+(128*MIDIpara2A)); }
+      MIDIpara1A=255; MIDIpara2A=255; } } }
+      
+  for (byte c=1;c<=12;c++) { if (peak[c].available()) { modulatorLevel[c]=peak[c].read()*envelopeLimit; } }
+  if (modulatorLevel[1] > carrierGain[1]) { carrierGain[1] /= envelopeSpeed; mixer3.gain(0, carrierGain[1]); }
+  if (modulatorLevel[1] < carrierGain[1]) { carrierGain[1] *= envelopeSpeed; mixer3.gain(0, carrierGain[1]); }
+  if (modulatorLevel[2] > carrierGain[2]) { carrierGain[2] /= envelopeSpeed; mixer3.gain(1, carrierGain[2]); }
+  if (modulatorLevel[2] < carrierGain[2]) { carrierGain[2] *= envelopeSpeed; mixer3.gain(1, carrierGain[2]); }
+  if (modulatorLevel[3] > carrierGain[3]) { carrierGain[3] /= envelopeSpeed; mixer4.gain(0, carrierGain[3]); }
+  if (modulatorLevel[3] < carrierGain[3]) { carrierGain[3] *= envelopeSpeed; mixer4.gain(0, carrierGain[3]); }
+  if (modulatorLevel[4] > carrierGain[4]) { carrierGain[4] /= envelopeSpeed; mixer4.gain(1, carrierGain[4]); }
+  if (modulatorLevel[4] < carrierGain[4]) { carrierGain[4] *= envelopeSpeed; mixer4.gain(1, carrierGain[4]); }
+  if (modulatorLevel[5] > carrierGain[5]) { carrierGain[5] /= envelopeSpeed; mixer5.gain(0, carrierGain[5]); }
+  if (modulatorLevel[5] < carrierGain[5]) { carrierGain[5] *= envelopeSpeed; mixer5.gain(0, carrierGain[5]); }
+  if (modulatorLevel[6] > carrierGain[6]) { carrierGain[6] /= envelopeSpeed; mixer5.gain(1, carrierGain[6]); }
+  if (modulatorLevel[6] < carrierGain[6]) { carrierGain[6] *= envelopeSpeed; mixer5.gain(1, carrierGain[6]); }
+  if (modulatorLevel[7] > carrierGain[7]) { carrierGain[7] /= envelopeSpeed; mixer3.gain(2, carrierGain[7]); }
+  if (modulatorLevel[7] < carrierGain[7]) { carrierGain[7] *= envelopeSpeed; mixer3.gain(2, carrierGain[7]); }
+  if (modulatorLevel[8] > carrierGain[8]) { carrierGain[8] /= envelopeSpeed; mixer3.gain(3, carrierGain[8]); }
+  if (modulatorLevel[8] < carrierGain[8]) { carrierGain[8] *= envelopeSpeed; mixer3.gain(3, carrierGain[8]); }
+  if (modulatorLevel[9] > carrierGain[9]) { carrierGain[9] /= envelopeSpeed; mixer4.gain(2, carrierGain[9]); }
+  if (modulatorLevel[9] < carrierGain[9]) { carrierGain[9] *= envelopeSpeed; mixer4.gain(2, carrierGain[9]); }
+  if (modulatorLevel[10] > carrierGain[10]) { carrierGain[10] /= envelopeSpeed; mixer4.gain(3, carrierGain[10]); }
+  if (modulatorLevel[10] < carrierGain[10]) { carrierGain[10] *= envelopeSpeed; mixer4.gain(3, carrierGain[10]); }
+  if (modulatorLevel[11] > carrierGain[11]) { carrierGain[11] /= envelopeSpeed; mixer5.gain(2, carrierGain[11]); }
+  if (modulatorLevel[11] < carrierGain[11]) { carrierGain[11] *= envelopeSpeed; mixer5.gain(2, carrierGain[11]); }
+  if (modulatorLevel[12] > carrierGain[12]) { carrierGain[12] /= envelopeSpeed; mixer5.gain(3, carrierGain[12]); }
+  if (modulatorLevel[12] < carrierGain[12]) { carrierGain[12] *= envelopeSpeed; mixer5.gain(3, carrierGain[12]); }
+  if (timer.check() == 1) { Serial.print(peak1.read()); Serial.print(" - "); Serial.print(peak2.read()); Serial.print(" - "); Serial.print(peak3.read()); Serial.println(""); logUtil(); } }
+
+void MIDIsetControl(byte channel, byte control, byte value) {
+  float fvalue, lvalue; fvalue=float(value)/127; lvalue=pow(fvalue,3);
+  if (control==0) { modulatorButtom=(lvalue*4950)+50; setModulatorFrequency(modulatorButtom,modulatorTop); }
+  if (control==1) { modulatorTop=(lvalue*4950)+50; setModulatorFrequency(modulatorButtom,modulatorTop); }
+  if (control==2) { carrierButtom=(lvalue*4950)+50; setCarrierFrequency(carrierButtom,carrierTop); }
+  if (control==3) { carrierTop=(lvalue*4950)+50; setCarrierFrequency(carrierButtom,carrierTop); }
+  if (control==4) { envelopeSpeed=((fvalue/2)+0.499); }
+  if (control==5) { envelopeLimit=fvalue; }
+  if (control==6) { setFilterResonance((fvalue*4.3)+0.7); }
+  if (control==7) { setOutputAmplifier(fvalue*5); }
+  if (control==8) {
+    if ((value&96)==0) { setModulatorSource(1,0,0); }
+    if ((value&96)==32) { setModulatorSource(0,1,0); }
+    if ((value&96)==64) { setModulatorSource(0,0,1); }
+    if ((value&96)==96) { setModulatorSource(0,0,1); } }
+  if (control==9) {
+    if ((value&96)==0) { setCarrierSource(1,0,0); }
+    if ((value&96)==32) { setCarrierSource(0,1,0); }
+    if ((value&96)==64) { setCarrierSource(0,0,1); }
+    if ((value&96)==96) { setCarrierSource(0,0,1); } } }
+
+void setFilterResonance(float r) { for (byte c=1;c<=12;c++) { filter11[c].resonance(r); filter12[c].resonance(r); filter21[c].resonance(r); filter22[c].resonance(r); } }
+
+void setOutputAmplifier(float g) { mixer6.gain(0,g); mixer6.gain(1,g); mixer6.gain(2,g); mixer6.gain(3,0); }
+
+void setModulatorFrequency(float b,float t) { for (byte c=1;c<=12;c++) { float freq=b*pow(t/b,(c-1)/11.0); filter11[c].frequency(freq); filter12[c].frequency(freq); } }
+
+void setCarrierFrequency(float b,float t) { for (byte c=1;c<=12;c++) { float freq=b*pow(t/b,(c-1)/11.0); filter21[c].frequency(freq); filter22[c].frequency(freq); } }
+
+void setModulatorSource(byte a, byte b, byte c) { mixer1.gain(0,a); mixer1.gain(1,b); mixer1.gain(2,c); mixer1.gain(3,0); }
+
+void setCarrierSource(byte a, byte b, byte c) { mixer2.gain(0,a); mixer2.gain(1,b); mixer2.gain(2,c); mixer2.gain(3,0); }
 
 void logUtil() {
-  Serial.println(AudioMemoryUsage()); Serial.println(AudioMemoryUsageMax());
-  Serial.println(AudioProcessorUsage()); Serial.println(AudioProcessorUsageMax()); }
+  Serial.print("Memory: "); Serial.print(AudioMemoryUsage()); Serial.print("/"); Serial.print(AudioMemoryUsageMax());
+  Serial.print(" CPU: "); Serial.print(AudioProcessorUsage()); Serial.print("/"); Serial.println(AudioProcessorUsageMax()); }
